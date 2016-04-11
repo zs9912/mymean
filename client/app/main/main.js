edb27beb21409d543ca34fd68160fae2da9ef4a2
Shu Zhang <shu@dhcp4208.datafax.com>
2016-04-11 11:24:38 -0400

    initial scaffolding

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanTutorialApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
