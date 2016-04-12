3b450029320c184f6d6824b957b036f61de07e4b
Shu Zhang <shu@dhcp4208.datafax.com>
2016-04-12 11:20:36 -0400

    add game and user model

--- server/config/seed.js
+++ server/config/seed.js
@@ -4,37 +4,40 @@
  */
 
 'use strict';
-import Thing from '../api/thing/thing.model';
+import Game from '../api/game/game.model';
+import User from '../api/user/user.model';
 
-Thing.find({}).remove()
+Game.find({}).remove()
   .then(() => {
-    Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Bower, Grunt, Babel, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, and Less.'
+    Game.create({
+      name: 'Halo 5',
+      platform: 'Xbox One',
+      genre: 'Shooter'
     }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
+      name: 'Fallout 4',
+      platform: 'PlayStation 4',
+      genre: 'Role-Playing'
     }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
+      name: 'Super Smash Bros',
+      platform: 'Wii U',
+      genre: 'Fighting'
     });
   });
 
+User.find({}).remove(function() {
+  User.create({
+    provider: 'local',
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+}, {
+    provider: 'local',
+    role: 'admin',
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin'
+}, function() {
+     console.log('finished populating users');
+   }
+ );
+});  
--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,8 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
+  app.use('/api/games', require('./api/game'));
+  app.use('/api/users', require('./api/user'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
